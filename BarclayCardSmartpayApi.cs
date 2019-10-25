65b8a2003e137b9872ee001354783b29e39d8357
tdonkor <tdonkor@gmail.com>
2019-10-25 10:27:29 +0100

    First Major save

--- BarclayCardSmartpayApi.cs
+++ BarclayCardSmartpayApi.cs
@@ -19,22 +19,25 @@ namespace BarclayCard_Smartpay
         // Data buffer for incoming data.
         byte[] bytes = new byte[1024];
 
-        //public BarclayCardSmartpayApi(int amount)
-        //{
-        //  //  this.amount = amount;
-       
-        //}
+     
+        public int TransNum { get; set; } = 0;
 
         public void SubmitPayment(int amount)
         {
-            // Data buffer for incoming data.  
-           // byte[] bytes = new byte[1024];
+        
             Random rnd = new Random();
-            int transNum = rnd.Next(1, int.MaxValue);
-            string submitResult = string.Empty;
+            TransNum = rnd.Next(1, int.MaxValue);
+
+            Console.WriteLine("Transaction Number is ***** " + TransNum +  " *****\n\n");
 
             XDocument payment = XDocument.Parse(
-                                   "<RLSOLVE_MSG version=\"5.0\">" + "<MESSAGE>" + "<TRANS_NUM>" + transNum + "</TRANS_NUM>" + "</MESSAGE>" + "<POI_MSG type=\"submittal\">" +
+                                   "<RLSOLVE_MSG version=\"5.0\">" +
+                                   "<MESSAGE>" +
+                                   //"<SOURCE_ID>K0001</SOURCE_ID>" +
+                                   "<TRANS_NUM>" + TransNum + 
+                                   "</TRANS_NUM>" +
+                                   "</MESSAGE>" +
+                                   "<POI_MSG type=\"submittal\">" +
                                     "<SUBMIT name=\"submitPayment\">" +
                                      "<TRANSACTION type= \"purchase\" action =\"auth\" source =\"icc\" customer=\"present\">" +
                                      "<AMOUNT currency=\"826\" country=\"826\">" +
@@ -44,80 +47,27 @@ namespace BarclayCard_Smartpay
                                     "</SUBMIT>" +
                                    "</POI_MSG>" +
                                  "</RLSOLVE_MSG>");
-           
-            try
+
+            CreateSocket(payment, "PAYMENT");
+
+        }
+
+        private string PaymentResult(string submitResult, int bytesRec)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(Encoding.ASCII.GetString(bytes, 0, bytesRec).ToString());
+            XmlNodeList result = doc.GetElementsByTagName("RESULT");
+
+            for (int i = 0; i < result.Count; i++)
             {
-                // Establish the remote endpoint for the socket.  
-                // This example uses port 8000 on the local computer.  
-                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-                IPAddress ipAddress = ipHostInfo.AddressList[0];
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+                // Console.WriteLine("Result: " + result[i].InnerXml);
+                if (result[i].InnerXml == "success")
+                    submitResult = "success";
 
-                // Create a TCP/IP  socket.  
-                Socket sender = new Socket(ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect the socket to the remote endpoint. Catch any errors.  
-                try
-                {
-                    sender.Connect(remoteEP);
-
-                    Console.WriteLine("Socket connected to {0}",
-                        sender.RemoteEndPoint.ToString());
-
-                    // Encode the data string into a byte array.  
-                    byte[] msg = Encoding.ASCII.GetBytes(payment.ToString());
-
-                    // Send the data through the socket.  
-                    int bytesSent = sender.Send(msg);
-
-                    // Receive the response from the remote device.  
-                    int bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("Payment Submitted = {0}",
-                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
-
-                    XmlDocument doc = new XmlDocument();
-                    doc.LoadXml(Encoding.ASCII.GetString(bytes, 0, bytesRec).ToString());
-                    XmlNodeList result = doc.GetElementsByTagName("RESULT");
-                    
-                    for (int i = 0; i < result.Count; i++)
-                    {
-                       // Console.WriteLine("Result: " + result[i].InnerXml);
-                        if (result[i].InnerXml == "success")
-                            submitResult = "success";
-                        Console.WriteLine("******Successful payment submitted******\n");
-                    }
-
-                    // Release the socket.  
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-
-                    if (submitResult == "success")
-                    {
-                    
-                        processTransaction(transNum);
-                        PrintReciptResponse(transNum);
-                        Finalise(transNum);
-                    }
-                }
-                catch (ArgumentNullException ane)
-                {
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    Console.WriteLine("SocketException : {0}", se.ToString());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
-                }
-
+                Console.WriteLine("******Successful payment submitted******\n");
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+
+            return submitResult;
         }
 
         void processTransaction(int transNum)
@@ -137,6 +87,7 @@ namespace BarclayCard_Smartpay
             CreateSocket(processTran, "PROCESSTRANSACTION");
 
         }
+
 
         public void PrintReciptResponse(int transNum)
         {
@@ -185,6 +136,8 @@ namespace BarclayCard_Smartpay
                 IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
                 IPAddress ipAddress = ipHostInfo.AddressList[0];
                 IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+                string submitResult = string.Empty;
+                string tranResp = string.Empty;
 
                 // Create a TCP/IP  socket.  
                 Socket sender = new Socket(ipAddress.AddressFamily,
@@ -194,8 +147,9 @@ namespace BarclayCard_Smartpay
                 try
                 {
                     sender.Connect(remoteEP);
+                   // Console.WriteLine("Connection is active 1: " + SocketConnected(sender));
 
-                    Console.WriteLine("\nSocket connected to:\n{0}",
+                    Console.WriteLine("\nSocket connected to:\n{0}\n",
                         sender.RemoteEndPoint.ToString());
 
                     // Encode the data string into a byte array.  
@@ -203,22 +157,66 @@ namespace BarclayCard_Smartpay
 
                     // Send the data through the socket.  
                     int bytesSent = sender.Send(msg);
-
+                 
                     // Receive the response from the remote device.  
                     int bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("\n" + operationStr + ":" +
+
+
+                    Console.WriteLine("\n" + operationStr + " :" +
                         "\n{0}",
                         Encoding.ASCII.GetString(bytes, 0, bytesRec));
 
+                    if (operationStr == "PAYMENT")
+                    {
+                        submitResult = PaymentResult(submitResult, bytesRec);
+                    }
+                    string outputResponse = string.Empty;
+                    
+
+                    //need to process until TransactionResponse is available so need to 
+                    // check the bytes returned until the TransactionResponse is recieved
+                    //and check for a merchant and customer receipt, if one is required send the transactiion
+                    // for this
                     if (operationStr == "PROCESSTRANSACTION")
                     {
-                        Thread.Sleep(10000);
+                        do
+                        {
+                            //check each response in turn
+                            //Console.WriteLine($" Count:{count++} is socket connected: {SocketConnected(sender)}");
+
+                            outputResponse = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                            //Thread.Sleep(1000);
+
+                            //recieve each response 
+                            bytesRec = sender.Receive(bytes);
+
+                            if (outputResponse.Contains("posPrintReceipt"))
+                            {
+                                Console.WriteLine("Printing Receipt:\n " + outputResponse + "\n");
+                                PrintReciptResponse(TransNum);
+                                Console.WriteLine("Printing Receipt2:\n " + outputResponse + "\n");
+                                PrintReciptResponse(TransNum);
+                                //Thread.Sleep(1000);
+                            }
+
+                        } while (outputResponse != string.Empty);
+
+
+                        Console.WriteLine("Finalise:\n " + outputResponse + "\n");
+                        Finalise(TransNum);
                     }
+
 
 
                     // Release the socket.  
                     sender.Shutdown(SocketShutdown.Both);
                     sender.Close();
+
+                    if (submitResult == "success")
+                    {
+                       processTransaction(TransNum);
+                    }
+                   
                 }
                 catch (ArgumentNullException ane)
                 {
@@ -240,7 +238,15 @@ namespace BarclayCard_Smartpay
             }
         }
 
-       
+        bool SocketConnected(Socket s)
+        {
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
 
         public void Dispose()
         {
